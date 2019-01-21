adf51bbfa31127d2e4e005a703e294531a86e1f5
Mohamad Zabad <zabadmohamad@yahoo.com>
2019-01-21 14:01:29 +0100

    Implemented Volley request to a standard url for testing purposes.

--- app/src/main/java/com/example/thezo/fyp/MainScreen.java
+++ app/src/main/java/com/example/thezo/fyp/MainScreen.java
@@ -7,11 +7,25 @@ import android.os.Looper;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkError;
+import com.android.volley.NoConnectionError;
+import com.android.volley.ParseError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.ServerError;
+import com.android.volley.TimeoutError;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
@@ -34,16 +48,62 @@ public class MainScreen extends AppCompatActivity {
     private long UPDATE_INTERVAL = 1 * 1000;  /* 1 sec */
     private long FASTEST_INTERVAL = 200; /* .2 sec */
     private TextView textView;
-    private int counter =0;
+    private int counter = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
 
-        startLocationUpdates();
+//        startLocationUpdates();
+        // Textview used to display the current location
         textView = findViewById(R.id.location);
 
+        // Post request to URL in order to update the location
 
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://www.google.com";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                // Display the first 500 characters of the response string.
+                Toast.makeText(MainScreen.this, "Connection made", Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                if (error instanceof TimeoutError) {
+                    Toast.makeText(MainScreen.this, "Error timeout!", Toast.LENGTH_SHORT).show();
+
+                }else if( error instanceof NoConnectionError) {
+                    Toast.makeText(MainScreen.this, "Error no connection!", Toast.LENGTH_SHORT).show();
+
+                } else if (error instanceof AuthFailureError) {
+                    Toast.makeText(MainScreen.this, "Error auth!", Toast.LENGTH_SHORT).show();
+
+                } else if (error instanceof ServerError) {
+                    Toast.makeText(MainScreen.this, "Error ServerError!", Toast.LENGTH_SHORT).show();
+
+                } else if (error instanceof NetworkError) {
+                    Toast.makeText(MainScreen.this, "Error Network!", Toast.LENGTH_SHORT).show();
+
+                } else if (error instanceof ParseError) {
+                    Toast.makeText(MainScreen.this, "Error Parse!", Toast.LENGTH_SHORT).show();
+
+                }else{
+                    Toast.makeText(MainScreen.this, "IDK FAM!", Toast.LENGTH_SHORT).show();
+
+                }
+
+
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 
     // Trigger new location updates at interval
@@ -67,9 +127,8 @@ public class MainScreen extends AppCompatActivity {
 
         // new Google API SDK v11 uses getFusedLocationProviderClient(this)
         if (ActivityCompat.checkSelfPermission(MainScreen.this,
-                ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED )
-        {
-            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
             return;
         }
         getFusedLocationProviderClient(MainScreen.this).requestLocationUpdates(mLocationRequest, new LocationCallback() {
@@ -81,13 +140,14 @@ public class MainScreen extends AppCompatActivity {
                 },
                 Looper.myLooper());
     }
+
     public void onLocationChanged(Location location) {
         // New location has now been determined
         String msg = "Updated Location: " +
                 Double.toString(location.getLatitude()) + "," +
                 Double.toString(location.getLongitude());
         //Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
-        textView.setText(msg+" "+counter);
+        textView.setText(msg + " " + counter);
         counter++;
         // You can now create a LatLng Object for use with maps
         LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
