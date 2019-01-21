6584bf572761be885855b01ae86d34dc94c2c868
Mohamad Zabad <zabadmohamad@yahoo.com>
2019-01-21 23:26:28 +0100

    New main activity (login activity) + basic changes

---
+++ app/src/main/java/com/example/thezo/fyp/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.thezo.fyp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    public String Title;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
