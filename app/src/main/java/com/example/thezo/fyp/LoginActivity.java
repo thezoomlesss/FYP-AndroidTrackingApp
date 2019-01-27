5c2caf82cd8ac16f2a9b613b3dab67dda3c0d890
Mohamad Zabad <c15745405@mydit.ie>
2019-01-27 15:47:09 +0000

    Implemented Volley requests into the Login Page
    So far, it's just pointing to the a basic test URL.
    Implementation with the REST API to be added.

--- app/src/main/java/com/example/thezo/fyp/LoginActivity.java
+++ app/src/main/java/com/example/thezo/fyp/LoginActivity.java
@@ -8,6 +8,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkError;
+import com.android.volley.NoConnectionError;
+import com.android.volley.ParseError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.ServerError;
+import com.android.volley.TimeoutError;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 public class LoginActivity extends AppCompatActivity {
 
     // The elements present in this activity
@@ -40,6 +53,51 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
+
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://www.google.com";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                // Display the first 500 characters of the response string.
+                Toast.makeText(LoginActivity.this, "Connection made", Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                if (error instanceof TimeoutError) {
+                    Toast.makeText(LoginActivity.this, "Error timeout!", Toast.LENGTH_SHORT).show();
+
+                }else if( error instanceof NoConnectionError) {
+                    Toast.makeText(LoginActivity.this, "Error no connection!", Toast.LENGTH_SHORT).show();
+
+                } else if (error instanceof AuthFailureError) {
+                    Toast.makeText(LoginActivity.this, "Error auth!", Toast.LENGTH_SHORT).show();
+
+                } else if (error instanceof ServerError) {
+                    Toast.makeText(LoginActivity.this, "Error ServerError!", Toast.LENGTH_SHORT).show();
+
+                } else if (error instanceof NetworkError) {
+                    Toast.makeText(LoginActivity.this, "Error Network!", Toast.LENGTH_SHORT).show();
+
+                } else if (error instanceof ParseError) {
+                    Toast.makeText(LoginActivity.this, "Error Parse!", Toast.LENGTH_SHORT).show();
+
+                }else{
+                    Toast.makeText(LoginActivity.this, "IDK FAM!", Toast.LENGTH_SHORT).show();
+
+                }
+
+
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
 
 
     }
