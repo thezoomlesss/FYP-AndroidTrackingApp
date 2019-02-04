36b7633c50bacdbc0ed9c9b56437aeb42e291c31
Mohamad Zabad <c15745405@mydit.ie>
2019-02-04 18:09:05 +0000

    Fixed XML of buttons that dissappeared

--- app/src/main/java/com/example/thezo/fyp/MapFragment.java
+++ app/src/main/java/com/example/thezo/fyp/MapFragment.java
@@ -9,6 +9,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -75,6 +76,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
@@ -82,6 +84,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         mView = inflater.inflate(R.layout.fragment_map, container, false);
+
         return mView;
     }
 
